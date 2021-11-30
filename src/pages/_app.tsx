e7f5f2ae39b3f9a83866d13dfe0216df40bcc3d4
KantaHasegawa <gyoza20000@gmail.com>
2021-11-30 16:30:55 +0900

    feat: Imprement Loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { CssBaseline, Box } from '@mui/material';
 import { AppProps } from 'next/app';
+import { Router } from 'next/router';
 import { SnackbarProvider } from 'notistack';
+import NProgress from 'nprogress';
 import { RecoilRoot } from 'recoil';
+import 'nprogress/nprogress.css';
 import '../../styels/global.css';
 import { SWRConfig } from 'swr';
 import AuthProvider from '../components/AuthProvider';
 import Footer from '../components/Footer';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
+
+  Router.events.on('routeChangeStart', (url) => {
+    NProgress.start();
+  });
+  Router.events.on('routeChangeComplete', () => NProgress.done());
+  Router.events.on('routeChangeError', () => NProgress.done());
+
   return (
     <RecoilRoot>
       <SWRConfig value={{ refreshInterval: 600000 }}>
